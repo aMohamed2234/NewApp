0d20ab229db0301cf3087298e44e554676386a56
ABDIRAUF <user@Abdirauf.ABDIRAUF>
2025-02-11 15:29:59 +0300

    created

---
+++ resources/views/events.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>This is Events Page</h1>
+
+    <div class="card bg-danger text-white p-5" style="width: 250px">
+        <div class="card-body">
+            <h5 class="card-title">Event 1</h5>
+            <p class="card-text">This is a event 1</p>
+            <a href="#" class="btn btn-light">View Details</a>
+            </div>
+    </div>
+</div>
+
+@endsection
